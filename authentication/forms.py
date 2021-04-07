d0b85b62bda2bb6d45e12dbede8f914369327b7e
jpupsik <patriotpava@gmail.com>
2021-04-07 23:47:16 +0300

    Hotfix

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,7 +2,8 @@ import unicodedata
 
 from django.contrib.auth.models import User
 from django.contrib.auth import password_validation
-from django.contrib.auth.forms import AuthenticationForm, UsernameField, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UsernameField, \
+    UserCreationForm
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
@@ -10,13 +11,13 @@ from django.utils.translation import gettext_lazy as _
 class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True,
                                                            'class': 'form-control',
-                                                           'id': 'username'},))
+                                                           'id': 'username'}, ))
     password = forms.CharField(
         label=_("Password"),
         strip=False,
         widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
                                           'class': 'form-control',
-                                          'id': 'password'}),)
+                                          'id': 'password'}), )
 
 
 class UsernameField(forms.CharField):
@@ -33,22 +34,23 @@ class UsernameField(forms.CharField):
         }
 
 
+# todo: Fix two not required fields in HTML (Password, Password confirmation)
 class RegisterForm(UserCreationForm):
-    password1 = forms.CharField(
-        label=_("Password"),
+    email = forms.CharField(
+        label=_("Email"),
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
-                                          'class': 'form-control',
-                                          'id': 'password1'}),
-        help_text=password_validation.password_validators_help_text_html(),
+        widget=forms.EmailInput(attrs={'autocomplete': 'on',
+                                       'class': 'form-control',
+                                       'id': 'email'}),
+        help_text=password_validation.password_validators_help_text_html(), # todo: Change it to email validation
     )
-    password2 = forms.CharField(
-        label=_("Password confirmation"),
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
+    password = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'off',
                                           'class': 'form-control',
-                                          'id': 'password2'}),
+                                          'id': 'password'}),
         strip=False,
-        help_text=_("Enter the same password as before, for verification."),
+        help_text=password_validation.password_validators_help_text_html(),
     )
 
     class Meta:

da30be2a6b7156005e603a9f9fcb5dbe0f50f5ad
ivan <wiglegpro@gmail.com>
2021-04-07 23:22:22 +0300

    Ability to style forms

--- authentication/views.py
+++ authentication/views.py
@@ -4,23 +4,25 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-from django.views.generic import TemplateView
+
+from authentication.forms import LoginForm, RegisterForm
 
 
 class CustomLoginView(LoginView):
+    form_class = LoginForm
     template_name = 'sign-in.html'
 
 
-class CustomLogOutView(TemplateView):
+class CustomLogOutView(LogoutView):
     template_name = 'log-out.html'
 
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'sign-up.html', {'form': UserCreationForm()})
+        return render(request, 'sign-up.html', {'form': RegisterForm()})
 
     def post(self, request):
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(reverse('login'))

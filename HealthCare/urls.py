34bd137b5ebe9e66520ceadb82f8eeb0b53cdd19
Maksym Hrynak <46346902+hrymasik@users.noreply.github.com>
2021-06-03 19:26:54 +0300

    Add files via upload
    Created test cases

--- HealthCare/urls.py
+++ HealthCare/urls.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from HealthCare.views import IndexView
+from reservations.views import doctor_list, ReservationView, reservation_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+    path('doctors/', doctor_list, name='doctor_list'),
+    path('reservations/', reservation_list, name='reservation_list'),
+    path('make-reserve/<int:doc_id>/', ReservationView.as_view(), name='make_reservation'),
     url('', IndexView.as_view())
 ]
---
+++ reservations/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class ReservationForm(forms.Form):
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
